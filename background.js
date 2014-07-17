64720d66a79bfc888e229ab01c93b1c6904393ff
Baker Bokorney <bbokorney@gmail.com>
2014-07-16 22:22:24 -0400

    Added support for SoundCloud and some extra error handling.

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
 chrome.commands.onCommand.addListener(function(command) {
+	// Grooveshark
 	chrome.tabs.query({url : "http://grooveshark.com/*"}, function(tabs){
-		chrome.tabs.executeScript(tabs[0].id, {code : "document.getElementById(\"play-pause\").click()"});
+		if(tabs[0]) {
+			chrome.tabs.executeScript(tabs[0].id, {code : "document.getElementById(\"play-pause\").click()"});
+		}
+	});
+	// SoundCloud
+	chrome.tabs.query({url: "https://soundcloud.com/*"}, function(tabs) {
+		if(tabs[0]) {
+			chrome.tabs.executeScript(tabs[0].id, {code : "document.getElementsByClassName(\"playControl sc-ir\")[0].click()"})
+		}
 	});
 });
